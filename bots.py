a660e12860a3557cbf2961dcdf760e72770b05e2
ktoto342245 <ktoto342245@gmail.com>
2025-05-01 16:08:30 +0300

    Update bots.py

--- bots.py
+++ bots.py
@@ -6,9 +6,24 @@ TOKEN = "7752116262:AAHW5JE9WCMftH8oH4rTUGmVaS35Dta72lM"  # Замініть н�
 
 # Готовий список користувачів
 USER_LIST = """
-@kall_help_bot @Helpmepls53 @zeyka09 @k0ly3 Zxc_top @naznaynepridumal @azaliya_103 
-@Virus_na @tromeozey Егор @Klaker_Ghost @Qvistyl @ItaliaAlexSlap @HAURFLOL 
-@Potrogaltravu @Zxcpsihuska @COBA_31 @TOR_7_77
+@kall_help_bot 
+@Helpmepls53 
+@zeyka09 
+@k0ly3 
+Zxc_top
+@naznaynepridumal 
+@azaliya_103 
+@Virus_na 
+@tromeozey 
+Егор 
+@Klaker_Ghost 
+@Qvistyl 
+@ItaliaAlexSlap 
+@HAURFLOL 
+@Potrogaltravu 
+@Zxcpsihuska 
+@COBA_31 
+@TOR_7_77
 """
 
 # Обробка текстових повідомлень
